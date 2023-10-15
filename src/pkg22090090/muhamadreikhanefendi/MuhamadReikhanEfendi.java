eb49a8beeb45467732a9da21b7a7c275c17adad1
MUHAMAD REIKHAN EFENDI <muhamadreikhan69@gmail.com>
2023-10-16 02:14:55 +0700

    first commit

---
+++ src/pkg22090090/muhamadreikhanefendi/MuhamadReikhanEfendi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg22090090.muhamadreikhanefendi;
+
+/**
+ *
+ * @author REIKHAN
+ */
+public class MuhamadReikhanEfendi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
